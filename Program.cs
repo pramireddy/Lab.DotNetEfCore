77fe695aa479309369bd2f21a41ac99d9d33128e
Prasad Ramireddy <pramireddy08@gmail.com>
2025-05-16 05:58:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Lab.DotNetEfCore._Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Program
+{
+    static async Task Main(string[] args)
+    {
+        var serviceProvider = new ServiceCollection()
+            //.AddDbContext<AppDbContext>(options =>
+            //    options.UseInMemoryDatabase("InMemoryDb")) // Note. InMemory does not support to run db commands in transaction
+            .AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer("YourConnectionString"))
+            .AddScoped(typeof(IRepository<>), typeof(Repository<>))
+            .AddScoped<IProductDataService, ProductDataService>() // Register Interface
+            .BuildServiceProvider();
+
+        var productService = serviceProvider.GetRequiredService<IProductDataService>();
+
+        // Add Product Using Service Layer
+        var newProduct = new Product { Id = 1, Name = "Laptop", Price = 1200 };
+        await productService.AddProductAsync(newProduct);
+
+        // Fetch Products Using Service Layer
+        var products = await productService.GetAllProductsAsync();
+        foreach (var product in products)
+        {
+            Console.WriteLine($"Product: {product.Name}, Price: {product.Price}");
+        }
+    }
+}
